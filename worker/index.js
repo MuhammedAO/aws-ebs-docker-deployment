6c5c187f4a22367e5239108ab4fa3a1e764e5922
Muhammed ogunsanya <mspeaks910@gmail.com>
2022-09-08 15:16:40 +0100

    index worker

--- worker/index.js
+++ worker/index.js
@@ -1,23 +1,19 @@
-const { redisHost, redisPort } = require('./keys')
-const { createClient } =  require('redis')
+const keys = require('./keys');
+const redis = require('redis');
 
+const redisClient = redis.createClient({
+  host: keys.redisHost,
+  port: keys.redisPort,
+  retry_strategy: () => 1000,
+});
+const sub = redisClient.duplicate();
 
-
-const redisClient = createClient({
-  host: redisHost,
-  port: redisPort,
-  retry_strategy: () => 1000
-})
-
-const redisSub = redisClient.duplicate()
-
-function fib(index){
- if(index < 2) return 1
- return fib(index - 1) + fib(index - 2)
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
 }
 
-redisSub.on('message', (channel, message) => {
-  redisClient.hset('values', message, fib(parseInt(message)))
-})
-
-redisSub.subscribe('insert');
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+});
+sub.subscribe('insert');
